d50ef7eb24d1aafe682aa11825e70ba8fbe94890
Daniel Öbrink <daniel@obrink.me>
2019-08-02 07:51:01 +0200

    Added files

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+def test_lynis_installed(host)
+    pkg = host.package("lynis")
+    assert pkg.is_installed
